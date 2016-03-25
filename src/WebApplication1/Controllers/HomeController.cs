f3e55ebef651f989bb94ca8acf93d92407e76b9b
Damon German <facmet107@gmail.com>
2016-03-25 12:12:01 -0500

    initial commit

---
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using PancakeModel;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            var rec = new ButtermilkPancakeRecipe(12);
+            ViewData["Title"] = "Buttermilk Pancakes!";
+            ViewData["Message"] = "Choose The amount of pancakes and we will generate the correct recipe.";
+
+            return View(rec);
+        }
+
+        public IActionResult Pancakes(decimal? NumberPancakes)
+        {
+            var rec = new ButtermilkPancakeRecipe(NumberPancakes??12);
+            return View(rec);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public static string PrintAAA()
+        {
+            return "dog nuts";
+          
+        }
+    }
+}
